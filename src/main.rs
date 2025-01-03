24dea617802ca8af25a18142a0731bb97ce43ab2
kangyou.choi <kangyou.choi@datastreams.co.kr>
2025-01-03 10:18:29 +0900

    main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use tracing::{info, Level};
+
+#[tokio::main]
+async fn main() {
+    // 로깅 설정
+    tracing_subscriber::fmt()
+        .with_max_level(Level::DEBUG)
+        .init();
+
+    info!("Zeta4g Graph Database Starting...");
 }
